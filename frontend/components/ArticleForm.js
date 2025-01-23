91ad73a0e29aeb4d7efbac8199e5d2900cf5d295
Michael Nobles <Mnobles33@gmail.com>
2025-01-23 11:00:52 -0700

    Next commit

--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -3,7 +3,7 @@ import PT from 'prop-types'
 
 const initialFormValues = { title: '', text: '', topic: '' }
 
-export default function ArticleForm(props) {
+export default function ArticleForm({ postArticle, updateArticle, setCurrentArticleId, currentArticle }) {
   const [values, setValues] = useState(initialFormValues)
   // ✨ where are my props? Destructure them here
 
@@ -12,7 +12,16 @@ export default function ArticleForm(props) {
     // Every time the `currentArticle` prop changes, we should check it for truthiness:
     // if it's truthy, we should set its title, text and topic into the corresponding
     // values of the form. If it's not, we should reset the form back to initial values.
-  })
+    if(currentArticle) {
+      setValues({
+        title: currentArticle.title,
+        text: currentArticle.text,
+        topic: currentArticle.topic
+      })
+    } else {
+      setValues(initialFormValues)
+    }
+  }, [currentArticle])
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -24,11 +33,23 @@ export default function ArticleForm(props) {
     // ✨ implement
     // We must submit a new post or update an existing one,
     // depending on the truthyness of the `currentArticle` prop.
+    if(currentArticle) {
+      updateArticle(currentArticle.article_id, values)
+    } else {
+      postArticle(values)
+    }
+    setCurrentArticleId(null)
+    setValues(initialFormValues)
   }
 
   const isDisabled = () => {
     // ✨ implement
     // Make sure the inputs have some values
+    return !(
+      values.title.replace(/\s/g, "").length >= 1 &&
+      values.text.replace(/\s/g, "").length >=1 &&
+      ['JavaScript', 'React', 'Node'].includes(values.topic)
+    );
   }
 
   return (
@@ -57,8 +78,22 @@ export default function ArticleForm(props) {
         <option value="Node">Node</option>
       </select>
       <div className="button-group">
-        <button disabled={isDisabled()} id="submitArticle">Submit</button>
-        <button onClick={Function.prototype}>Cancel edit</button>
+        {
+          currentArticle
+          ? (
+            <>
+              <button disabled={isDisabled()}
+              id="submitArticle">Submit</button>
+              <button disabled={isDisabled()} 
+              onClick={() => setCurrentArticleId(null)}
+              id="cancelEdit">Cancel</button>
+            </>
+          )
+          : (
+            <button disabled={isDisabled()}
+            id="submitArticle">Submit</button>
+          )
+        }
       </div>
     </form>
   )
