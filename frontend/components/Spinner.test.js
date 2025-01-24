a3d3dda0a5c44625b5c78c86b5a523a69098adf4
Michael Nobles <Mnobles33@gmail.com>
2025-01-24 10:39:51 -0700

    Next commit

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,7 +1,7 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 import React from 'react';
-import { render } from 'testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Spinner from './Spinner.js';
 
